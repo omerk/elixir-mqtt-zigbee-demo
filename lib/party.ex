5b9fad0099f8e88574c14a35920915c8ace18408
Omer Kilic <omerkilic@gmail.com>
2021-11-04 18:55:08 +0000

    last minute cleanup

---
+++ lib/party.ex
@@ -0,0 +1,47 @@
+defmodule Party do
+  use GenServer
+  require Logger
+
+  @device "party-bulb"
+
+  def child_spec(opts) do
+    %{
+      id: Keyword.get(opts, :name, __MODULE__),
+      start: {__MODULE__, :start_link, [opts]},
+      restart: Keyword.get(opts, :restart, :transient),
+      type: :worker
+    }
+  end
+
+  def start_link(_opts \\ []) do
+    Logger.debug("blink started")
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  def init([]) do
+    send_toggle()
+    {:ok, :on}
+  end
+
+  def handle_info(:toggle, :on) do
+    MqttServer.send_message_async_to_topic("zigbee2mqtt/" <> @device <> "/set/state", "OFF")
+    send_toggle()
+    {:noreply, :off}
+  end
+
+  def handle_info(:toggle, :off) do
+    MqttServer.send_message_async_to_topic("zigbee2mqtt/" <> @device <> "/set/state", "ON")
+    send_toggle()
+    {:noreply, :on}
+  end
+
+  def handle_info(msg, state) do
+    Logger.debug("catch all: #{inspect(msg)}")
+    {:noreply, state}
+  end
+
+  def send_toggle() do
+    Process.send_after(self(), :toggle, 750)
+  end
+
+end
